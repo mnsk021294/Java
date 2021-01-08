8b0c688cdccf84cd7b9be6ce1bb968ccb4bda507
mnsko <jastikrishna777@gmail.com>
2021-01-08 00:13:59 -0500

    Java program

---
+++ Calculator/src/Calc.java
@@ -0,0 +1,24 @@
+
+public class Calc {
+
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println("Addition :"+(a+b));
+		System.out.println("Substraction :"+(a-b));
+		System.out.println("Multiplication :"+(a*b));
+		
+		double c=(a/b);
+		
+	
+		System.out.println("Division :"+c);
+		
+ 
+	
+	
+	
+	}
+}
